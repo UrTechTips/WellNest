c28e3dc49518027bc4c6071940198bd9210335eb
UrTechTips <66728798+UrTechTips@users.noreply.github.com>
2024-11-10 08:43:29 +0530

    minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -33,12 +33,12 @@ export default function Page() {
 						<h1 className="text-balance text-5xl font-semibold tracking-tight text-gray-100 sm:text-7xl">Personalized Wellness Tips, Powered by AI</h1>
 						<p className="mt-8 text-pretty text-lg font-medium mx-auto max-w-3xl text-gray-300 sm:text-xl/8">Personalized health tips based on your local weather. From hydration reminders to clothing advice, stay prepared every day.</p>
 						<div className="mt-10 flex items-center justify-center gap-x-6">
-							<a href="#" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+							<a href="/login" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
 								Get My Wellness Tips
 							</a>
-							<a href="/login" className="text-sm/6 font-semibold text-gray-200">
+							{/* <a href="/login" className="text-sm/6 font-semibold text-gray-200">
 								Learn How It Works <span aria-hidden="true">→</span>
-							</a>
+							</a> */}
 						</div>
 					</div>
 				</div>
--- src/components/Navbar.component.js
+++ src/components/Navbar.component.js
@@ -1,6 +1,7 @@
 import { UserContext } from "@/app/layout";
 import { auth } from "@/configs/firebase";
 import { signOut } from "firebase/auth";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useContext } from "react";
 
@@ -23,11 +24,11 @@ const Navbar = () => {
 		<header className="absolute inset-x-0 top-0 z-50">
 			<nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
 				<div className="flex lg:flex-1">
-					<a href="#" className="-m-1.5 p-1.5 flex items-center">
+					<Link href="/" className="-m-1.5 p-1.5 flex items-center">
 						<span className="sr-only">WellNest</span>
 						<img alt="WellNest Logo" src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600" className="h-8 w-auto mr-2" />
 						<span className="text-lg font-bold text-indigo-600">WellNest Dashboard</span>
-					</a>
+					</Link>
 				</div>
 
 				<div className=" lg:flex lg:flex-1 lg:justify-end cursor-pointer" onClick={handleClick}>
