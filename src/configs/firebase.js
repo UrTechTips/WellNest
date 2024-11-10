f8cd8eb1acc3827100cd35e87c7b17ef2181e916
UrTechTips <66728798+UrTechTips@users.noreply.github.com>
2024-11-10 09:08:38 +0530

    Secured api keys

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -2,22 +2,22 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// const apiKey = process.env.NEXT_PUBLIC_FIREBASE_apiKey;
-// const authDomain = process.env.NEXT_PUBLIC_FIREBASE_authDomain;
-// const projectId = process.env.NEXT_PUBLIC_FIREBASE_projectId;
-// const storageBucket = process.env.NEXT_PUBLIC_FIREBASE_storageBucket;
-// const messagingSenderId = process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId;
-// const appId = process.env.NEXT_PUBLIC_FIREBASE_appId;
+const apiKey = process.env.NEXT_PUBLIC_FIREBASE_apiKey;
+const authDomain = process.env.NEXT_PUBLIC_FIREBASE_authDomain;
+const projectId = process.env.NEXT_PUBLIC_FIREBASE_projectId;
+const storageBucket = process.env.NEXT_PUBLIC_FIREBASE_storageBucket;
+const messagingSenderId = process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId;
+const appId = process.env.NEXT_PUBLIC_FIREBASE_appId;
 
 // console.log(apiKey);
 
 const firebaseConfig = {
-	apiKey: "AIzaSyA_pvVp7hstoCPiX7YzoQlsZirMUFyf9xM",
-	authDomain: "wellnest-b5ba3.firebaseapp.com",
-	projectId: "wellnest-b5ba3",
-	storageBucket: "wellnest-b5ba3.firebasestorage.app",
-	messagingSenderId: "250599569566",
-	appId: "1:250599569566:web:875e7a24a87053b45ba38f",
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_apiKey,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_authDomain,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_projectId,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_storageBucket,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_messagingSenderId,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_appId,
 };
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
